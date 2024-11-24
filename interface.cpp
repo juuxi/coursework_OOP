26156a8b72e818c582380e6bd8221b88013f693b
Dmitry Shopyrev <dimashopyrev@mail.ru>
2024-11-24 10:56:55 +0300

    grid for 5 windows

--- interface.cpp
+++ interface.cpp
@@ -4,15 +4,10 @@
 TParameters::TParameters(QWidget *parent)
     : QWidget(parent)
 {
-    setWindowTitle("Не главное окно");
-    setFixedSize(500, 300);
-    btn_1 = new QPushButton(this);
-    btn_1->setGeometry(0, 0, 120, 50);
 }
 
 TParameters::~TParameters()
 {
-    delete btn_1;
 }
 
 void TParameters::show_parameters()
@@ -20,21 +15,71 @@ void TParameters::show_parameters()
     this->show();
 }
 
+TTableVisual::TTableVisual(QWidget *parent)
+    : QWidget(parent)
+{
+    tag = new QLabel("Table", this);
+    tag->setGeometry(0, 0, 100, 30);
+}
+
+TTableVisual::~TTableVisual()
+{
+    delete tag;
+}
+
+TTableControl::TTableControl(QWidget *parent)
+    : QWidget(parent)
+{
+    tag = new QLabel("Table Control", this);
+    tag->setGeometry(0, 0, 100, 30);
+}
+
+TTableControl::~TTableControl()
+{
+    delete tag;
+}
+
+TRackVisual::TRackVisual(QWidget *parent)
+    : QWidget(parent)
+{
+    tag = new QLabel("Rack", this);
+    tag->setGeometry(0, 0, 100, 30);
+}
+
+TRackVisual::~TRackVisual()
+{
+    delete tag;
+}
+
+TRackControl::TRackControl(QWidget *parent)
+    : QWidget(parent)
+{
+    tag = new QLabel("Rack Control", this);
+    tag->setGeometry(0, 0, 100, 30);
+}
+
+TRackControl::~TRackControl()
+{
+    delete tag;
+}
+
 TInterface::TInterface(QWidget *parent)
     : QWidget(parent)
 {
     setWindowTitle("Главное окно");
     setFixedSize(1500, 800);
-    l = new QGridLayout(this);
+    layout = new QGridLayout(this);
     param = new TParameters();
-    param2 = new TParameters();
-    param3 = new TParameters();
-    param4 = new TParameters();
-    l->addWidget(param,0,0);
-    l->addWidget(param2,1,0);
-    l->addWidget(param3,0,1);
-    l->addWidget(param4,1,1);
-    this->setLayout(l);
+    table_visual = new TTableVisual();
+    table_control = new TTableControl();
+    rack_visual = new TRackVisual();
+    rack_control = new TRackControl();
+    layout->addWidget(param,0,0);
+    layout->addWidget(table_visual,0,1);
+    layout->addWidget(table_control,1,1);
+    layout->addWidget(rack_visual,0,2);
+    layout->addWidget(rack_control,1,2);
+    this->setLayout(layout);
 }
 
 TInterface::~TInterface()
--- interface.h
+++ interface.h
@@ -2,13 +2,14 @@
 #define TINTERFACE_H
 
 #include <QWidget>
+#include <QLabel>
+#include <QLineEdit>
 #include <QGridLayout>
 #include <QPushButton>
 
 class TParameters : public QWidget
 {
     Q_OBJECT
-    QPushButton* btn_1;
 
 public:
     TParameters(QWidget *parent = nullptr);
@@ -17,14 +18,55 @@ public slots:
     void show_parameters();
 };
 
+class TTableVisual : public QWidget
+{
+    Q_OBJECT
+    QLabel* tag;
+
+public:
+    TTableVisual(QWidget *parent = nullptr);
+    ~TTableVisual();
+};
+
+class TTableControl : public QWidget
+{
+    Q_OBJECT
+    QLabel* tag;
+
+public:
+    TTableControl(QWidget *parent = nullptr);
+    ~TTableControl();
+};
+
+class TRackVisual : public QWidget
+{
+    Q_OBJECT
+    QLabel* tag;
+
+public:
+    TRackVisual(QWidget *parent = nullptr);
+    ~TRackVisual();
+};
+
+class TRackControl : public QWidget
+{
+    Q_OBJECT
+    QLabel* tag;
+
+public:
+    TRackControl(QWidget *parent = nullptr);
+    ~TRackControl();
+};
+
 class TInterface : public QWidget
 {
     Q_OBJECT
     TParameters* param;
-    TParameters* param2;
-    TParameters* param3;
-    TParameters* param4;
-    QGridLayout   *l;
+    TTableVisual* table_visual;
+    TTableControl* table_control;
+    TRackVisual* rack_visual;
+    TRackControl* rack_control;
+    QGridLayout   *layout;
 
 public:
     TInterface(QWidget *parent = nullptr);
